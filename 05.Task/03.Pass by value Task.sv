cd8de38db081e3241df83555b7942b885bfc3fa0
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-03 15:47:54 +0530

    Create 03.Pass by value Task.sv

---
+++ 05.Task/03.Pass by value Task.sv
@@ -0,0 +1,18 @@
+module pass_by_value_task;
+  int a, b;
+  int out;
+  
+  
+  task multiply(int a,b, output int out);
+    a = a*b; 
+    out = a;
+  endtask
+  
+  initial begin
+	a=10;
+    b=5;
+
+    multiply(a,b, out);
+    $display("Task: out = %0d for a = %0d and b = %0d", out, a, b);
+  end
+endmodule
