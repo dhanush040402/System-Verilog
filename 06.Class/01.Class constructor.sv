66bf776c8976a1589a02e252f540ccba277e0acc
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-06 18:10:37 +0530

    Create 01.Class constructor.sv

---
+++ 06.Class/01.Class constructor.sv
@@ -0,0 +1,16 @@
+class transaction;
+  bit [31:0] data;
+  int id;
+  
+  function new (bit [31:0] m_data);
+    $display("Inside constructor");
+    data = m_data;
+  endfunction
+endclass
+
+module class_example;
+  initial begin
+    transaction tr = new(10);
+    $display("Value of data = %0h", tr.data);
+  end
+endmodule
