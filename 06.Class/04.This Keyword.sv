37efab9f3fc13e4c142c88c03c1f8514c5fc228d
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-06 19:02:44 +0530

    Update 04.This Keyword.sv

--- 06.Class/04.This Keyword.sv
+++ 06.Class/04.This Keyword.sv
@@ -1,22 +1,23 @@
-class base_class;
-  string fan ="OFF" ; 
-  string switch="OFF"; 
-   
-  function void open_electricity();
-    string fan="ON";
-    string switch="ON";
-    this.fan=fan;
-    this.switch=switch;
-    $display("Inside class method :- switch is %0s that's why fan is %0s",switch,fan);
-  endfunction:open_electricity
-endclass:base_class
+class transaction;
+  int id;
+  bit [31:0] data;
 
-module check_electricity;
-  base_class b1;
-  
-  initial begin:BEGIN_I
-    b1=new();
-    b1.open_electricity;
-    $display("Outside class :- switch is %0s that's why fan is %s",b1.switch,b1.fan);
-  end:BEGIN_I
-endmodule:check_electricity
+  function new(int id, bit [31:0] data);
+    this.id   = id;     
+    this.data = data;   
+  endfunction
+
+  function void print();
+    $display("id = %0d, data = %0h", this.id, this.data);
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    transaction tr;
+
+    tr = new(5, 32'hABCD);
+    tr.print();
+  end
+endmodule
+
