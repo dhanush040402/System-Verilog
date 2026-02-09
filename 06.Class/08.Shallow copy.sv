b74315ec077a91b9272a7e302c2c9b96ab9b30cb
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-09 13:13:58 +0530

    Create 08.Shallow copy.sv

---
+++ 06.Class/08.Shallow copy.sv
@@ -0,0 +1,19 @@
+class Packet;
+  int addr;
+endclass
+
+module shallow_copy;
+Packet p1, p2;
+
+initial begin
+  p1 = new();
+  p1.addr = 32'hAA;
+
+  p2 = new p1;   
+
+  p2.addr = 32'hBB;
+
+  $display("p1.addr = %h", p1.addr);
+  $display("p2.addr = %h", p2.addr); 
+end
+endmodule
