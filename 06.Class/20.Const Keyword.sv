53ffb452b416a70dc6629ecd13ce563abd29102e
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-13 11:04:36 +0530

    Create 20.Const Keyword.sv

---
+++ 06.Class/20.Const Keyword.sv
@@ -0,0 +1,19 @@
+class transaction;
+  bit [31:0] data;
+  const int id = 1;
+  
+  function void display();
+    $display("data = %0d and id = %0d", data, id);
+  endfunction
+endclass
+
+module class_example;
+  transaction tr;
+  
+  initial begin
+    tr = new();
+    tr.data = 100;
+    tr.id = 2;  // Invalid const usage
+    tr.display();
+  end
+endmodule
